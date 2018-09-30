71e4725d318adeb070174a9f556831a28e656a45
Ron Aebig <tankstrr@gmail.com>
2018-09-30 17:46:56 -0600

    first test, trying to nail down growth rates

--- lifeSim Basic/lifeSim Basic/Program.cs
+++ lifeSim Basic/lifeSim Basic/Program.cs
@@ -11,6 +11,22 @@ namespace lifeSim_Basic
         static void Main(string[] args)
         {
             Console.WriteLine("lifeSim 0.1 by tankstrr");
+            Console.WriteLine();
+            int startPop = 10000;
+            int startYear = -200000;
+
+            double currPop = startPop;
+            int currYear = startYear;
+            int endYear = 2000;
+            double growthRate = 0.0001;
+            while (currYear < endYear)
+            {
+                currPop = currPop + (currPop * growthRate);
+                Console.WriteLine("Year: {0}         Pop: {1:N0}", currYear, currPop);
+                currYear++;
+            }
+            Console.WriteLine("..press any key to exit");
+            Console.ReadKey();
         }
     }
 }
