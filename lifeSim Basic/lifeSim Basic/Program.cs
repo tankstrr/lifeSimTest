d8c9a521bf4061962e0c8f842c951f141fbcac31
Ron Aebig <tankstrr@gmail.com>
2018-09-30 17:32:50 -0600

    First Commit

---
+++ lifeSim Basic/lifeSim Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lifeSim_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lifeSim 0.1 by tankstrr");
+        }
+    }
+}
